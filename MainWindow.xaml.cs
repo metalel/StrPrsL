19a74ae769ef89c2856d37ff5c70827b7eb1d9fd
metalel <oyunzo3@hotmail.com>
2023-12-04 04:50:29 +0300

    UI created
    UI created using https://github.com/AngryCarrot789/WPFDarkTheme, AvalonEdit (http://avalonedit.net/), Extended Wpf Toolkit (https://github.com/xceedsoftware/wpftoolkit), ModernWpfUI (https://github.com/Kinnara/ModernWpf)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ICSharpCode.AvalonEdit;
 
 namespace StrPrsL
 {
@@ -23,6 +26,15 @@ namespace StrPrsL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("StrPrsL.Scripting.SyntaxHighlighting.xshd"))
+            {
+                System.Xml.XmlTextReader reader = new System.Xml.XmlTextReader(stream);
+                this.scriptEditor.SyntaxHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
+            }
         }
     }
 }
